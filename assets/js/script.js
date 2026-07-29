d9a6ed8376b3baf1c68ab4313e3fbd415229d7bb
Didier AGT <didieragt@gmail.com>
2026-07-29 16:11:11 -0500

    slot-7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,16 @@
-const menuBtn = document.getElementById('menu-btn');
-const mobileMenu = document.getElementById('mobile-menu');
-const iconHamburger = document.getElementById('icon-hamburger');
-const iconClose = document.getElementById('icon-close');
-const header = document.getElementById('header');
+const cardsUno = document.getElementsByClassName('card')
 
-let isScrolling = false;
+document.addEventListener('DOMContentLoaded', () => {
 
+    let indice = 1;
 
-window.addEventListener('scroll', () => {
-    if (!isScrolling) {
-        window.requestAnimationFrame(() => {
-            const isPastThreshold = window.scrollY > 20;
-
-            header.classList.toggle('bg-font-200/60', isPastThreshold);
-            header.classList.toggle('transition-transform', isPastThreshold);
-            header.classList.toggle('duration-500', isPastThreshold);
-            header.classList.toggle('bg-transparent', !isPastThreshold);
-
-            isScrolling = false;
-        });
-
-        isScrolling = true;
-    }
-});
-
-
-//Carrusel
-
-document.addEventListener("DOMContentLoaded", () => {
-    const carrusel = document.getElementById('carrusel');
-    carrusel.innerHTML += carrusel.innerHTML;
-
-    let posicion = 0;
-    const velocidad = 1;
-
-    function animar() {
-        posicion += velocidad;
-
-        if (posicion >= carrusel.scrollWidth / 2) {
-            posicion = 0;
+    function cambiarImagen() {
+        indice++;
+        if (indice >= cardsUno.length) {
+            indice = 0;
         }
-
-        carrusel.style.transform = `translateX(-${posicion}px)`;
-        requestAnimationFrame(animar);
+        console.log(cardsUno[indice])
     }
 
-    animar();
-})
-
-
-AOS.init({
-    duration: 2000,
+    //setInterval(cambiarImagen, 3000);
 })
